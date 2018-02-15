b23bf67d88f817a69abb36d8db22052874dc32c0
alaaDragneel <m.alaa@orbscope.com>
2018-02-15 04:20:43 +0200

    start project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBSRvnBMqSd75L0-pthpD4DKW5YNWS1N_c",
+        authDomain: "angular-my-shop.firebaseapp.com",
+        databaseURL: "https://angular-my-shop.firebaseio.com",
+        projectId: "angular-my-shop",
+        storageBucket: "angular-my-shop.appspot.com",
+        messagingSenderId: "498734155798"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBSRvnBMqSd75L0-pthpD4DKW5YNWS1N_c",
+        authDomain: "angular-my-shop.firebaseapp.com",
+        databaseURL: "https://angular-my-shop.firebaseio.com",
+        projectId: "angular-my-shop",
+        storageBucket: "angular-my-shop.appspot.com",
+        messagingSenderId: "498734155798"
+    }
 };
